de3a44762d55674c32efd4603fb0b8cf8ac07221
lenovo-pc <448267696@qq.com>
2023-06-18 12:26:09 +0800

    update

--- testNotify.js
+++ testNotify.js
@@ -10,40 +10,44 @@ const timeout = 15000;
 // QingLong log path
 const qlLogPath = '../log/';
 const timeStamp = getTodayDateStr();
-let timeStampRead;
+let lastNotifyTimeStamp = getTodayDateStr(0, -7); //上次通知时间 now - 1hour
 
 //启用通知的项目
 let NOTIFY_PRO = ['6dylan6_jdpro'];
 
-let MODE = 0;// 0:WHITE 1:BLACK
+let MODE = 0; // 0:WHITE 1:BLACK
 let WHITELIST = [
     'jd_fruit#1',
     'jd_speed_sign#0',
-    'jd_speed_signfree#2',
+    //    'jd_speed_signfree#2',
 ];
 
 let WHITELIST_MODE = [
-    { 'start': "系统通知", 'end': ', 结束', 'block': [] },
-    { 'start': "系统通知", 'end': '开始【京东账号', 'block': [] },
-    { 'start': "开始【京东账号", 'end': '系统通知', 'block': ['脚本也许随时失效,请注意', '没有需要签到的'] },
-]
+    { start: '系统通知', end: ', 结束', block: [] },
+    { start: '系统通知', end: '开始【京东账号', block: [] },
+    {
+        start: '开始【京东账号',
+        end: '系统通知',
+        block: ['脚本也许随时失效,请注意', '没有需要签到的'],
+    },
+];
 
 //日志黑名单
 let BLACKLIST = [
     'testNotify',
     'jd_dreamFactory',
-    'jd_jxmc',//jx牧场
-    'jd_cfd',//财富岛
-    'jd_jdfactory',//dd工厂
-    'jd_wish',//
-    'jd_beauty',//
-    'jd_xinruimz',//
-    'jd_mf_new',//魔方
-    'jd_superBrandZII',//特务
-    'jd_superBrandStar',//特务
-    'jd_twjk_new.js',//特务
-    'jd_SuperBrandJXZ',//特务
-    'jd_sign_graphics1',//特务
+    'jd_jxmc', //jx牧场
+    'jd_cfd', //财富岛
+    'jd_jdfactory', //dd工厂
+    'jd_wish', //
+    'jd_beauty', //
+    'jd_xinruimz', //
+    'jd_mf_new', //魔方
+    'jd_superBrandZII', //特务
+    'jd_superBrandStar', //特务
+    'jd_twjk_new.js', //特务
+    'jd_SuperBrandJXZ', //特务
+    'jd_sign_graphics1', //特务
 ];
 
 //level 0：简易通知，只显示知否完成
@@ -51,63 +55,68 @@ let BLACKLIST = [
 const GLOBAL_LEVEL_0 = 0;
 let GLOBAL_LEVEL = GLOBAL_LEVEL_0;
 
-
 // test
 // processLineByLine(__dirname + '/2023-03-13-06-18-13.log')
 // processLineByLine(__dirname + '/jd_speed_signfree.log')
 // prod
-taskStart()
+taskStart();
 
 function taskStart() {
     fs.readFile(__dirname + '/notifyTimeLog.txt', function (err, data) {
-        if (err) {
-            console.log(err);
-            // timeStampRead = getTodayDateStr(1, -2);
-        } else {
-            const arr = data.toString().replace(/\r\n/g, '\n').split('\n');
+        let arr = []
+        if (!err) {
+            arr = data.toString().replace(/\r\n/g, '\n').split('\n');
             console.log(arr);
+
+            //循环原因：取最后一个有效的时间
             for (let i = arr.length - 1; i >= 0; i--) {
                 if (arr[i] && arr[i].length > 4) {
-                    timeStampRead = arr[i];//倒序选择第一个时间
-                    // timeStampRead = '2022-11-23-15-12-46';
+                    lastNotifyTimeStamp = arr[i]; //倒序选择第一个时间
+                    // lastNotifyTimeStamp = '2022-11-23-15-12-46';
                     break;
                 }
             }
-
-            if (timeStampRead) {
-                console.log('上次通知的时间戳: ' + timeStampRead)
-                loopLogDirs();
-            } else {
-                console.error('错误的时间戳: ' + timeStampRead)
-            }
-            writeExeTime(arr.length > 50 ? 'w' : 'a');
         }
+
+        console.log('上次通知的时间戳: ' + lastNotifyTimeStamp);
+        loopLogDirs();
+
+        writeExeTime(arr.length > 50 ? 'w' : 'a');
     });
 }
 
-
 function writeExeTime(f) {
-    // 将数据写入文件sample.html
-    fs.writeFile(__dirname + '/notifyTimeLog.txt', timeStamp + os.EOL, { flag: f },
+    // 将执行日期数据写入文件notifyTimeLog.txt
+    fs.writeFile(
+        __dirname + '/notifyTimeLog.txt',
+        timeStamp + os.EOL,
+        { flag: f },
         // 写入文件后调用的回调函数
         function (err) {
             if (err) throw err;
             // 如果没有错误
-            console.log('本次通知时间写入成功: ' + timeStamp)
-        });
+            console.log('本次通知时间写入成功: ' + timeStamp);
+        }
+    );
 }
 
 function loopLogDirs() {
     fs.readdir(qlLogPath, (err, LogDirs) => {
         if (!err) {
             //hit need notify project list
-            let filterLogDirs = LogDirs.filter(curLogFolder => hitNotifyPro(curLogFolder, NOTIFY_PRO));
+            let filterLogDirs = LogDirs.filter((curLogFolder) =>
+                hitNotifyPro(curLogFolder, NOTIFY_PRO)
+            );
 
             //filter white/black list
             if (MODE == 0) {
-                filterLogDirs = filterLogDirs.filter(curLogFolder => filterList(curLogFolder, WHITELIST));
+                filterLogDirs = filterLogDirs.filter((curLogFolder) =>
+                    filterList(curLogFolder, WHITELIST)
+                );
             } else {
-                filterLogDirs = filterLogDirs.filter(curLogFolder => !filterList(curLogFolder, BLACKLIST));
+                filterLogDirs = filterLogDirs.filter(
+                    (curLogFolder) => !filterList(curLogFolder, BLACKLIST)
+                );
             }
 
             // console.log(filterLogDirs);
@@ -116,7 +125,13 @@ function loopLogDirs() {
                 console.log('没有扫描到需要发送的通知');
                 return;
             }
-            console.log('will scan log folder nums:' + len + ', base log folder is:[' + NOTIFY_PRO + ']');
+            console.log(
+                'will scan log folder nums:' +
+                len +
+                ', base log folder is:[' +
+                NOTIFY_PRO +
+                ']'
+            );
             for (let i = 0; i < len; i++) {
                 let folderName = filterLogDirs[i];
                 readLogFilesInDir(folderName);
@@ -140,7 +155,7 @@ function readLogFilesInDir(folderName) {
                 logTime2 = logTime1;
             }
 
-            if (logTime2 > timeStampRead) {
+            if (logTime2 > lastNotifyTimeStamp) {
                 let singleLogPath = qlLogPath + folderName + '/' + logTime2;
                 // console.log(singleLogPath);
                 readSingleLog(singleLogPath, folderName);
@@ -151,8 +166,6 @@ function readLogFilesInDir(folderName) {
     });
 }
 
-
-
 // readFile line by line
 async function processLineByLine(filePath) {
     console.log(filePath);
@@ -160,7 +173,7 @@ async function processLineByLine(filePath) {
 
     const rl = readline.createInterface({
         input: fileStream,
-        crlfDelay: Infinity
+        crlfDelay: Infinity,
     });
     // Note: we use the crlfDelay option to recognize all instances of CR LF
     // ('\r\n') in input.txt as a single line break.
@@ -185,7 +198,7 @@ async function processLineByLine(filePath) {
         if (mode.block && mode.block.length > 0) {
             let blockArr = mode.block;
 
-            if (blockArr.some(x => line.indexOf(x) > -1)) {
+            if (blockArr.some((x) => line.indexOf(x) > -1)) {
                 continue;
             }
         }
@@ -212,30 +225,34 @@ async function processLineByLine(filePath) {
         }
 
         if (line.indexOf('执行结束') > -1) {
-            console.log('遍历日志结束')
+            console.log('遍历日志结束');
             obj.endLine = line;
         }
     }
-    console.log("输出结果：", obj);
-    BarkNotify(getTodayDateStr(2) + ' ' + obj.title, obj.notifyContent + obj.endLine);
+    console.log('输出结果：', obj);
+    BarkNotify(
+        getTodayDateStr(2) + ' ' + obj.title,
+        obj.notifyContent + obj.endLine
+    );
 }
 
 function whichMode(filePath) {
     for (const item of WHITELIST) {
         let name = item.split('#')[0];
         let modeIndex = item.split('#')[1];
-        console.log(name, modeIndex)
+        console.log(name, modeIndex);
         if (filePath.indexOf(name + '/') > -1) {
-            return WHITELIST_MODE[modeIndex]
+            return WHITELIST_MODE[modeIndex];
         }
     }
 }
 
-// format 0/undefined 2023-03-19 11:40:23
+// format 0/undefined fullformat 2023-03-19-11-40-23
 // format 1 : 2023-03-19
 // format 2 : 03-19
 function getTodayDateStr(format, offset) {
-    let need_year = true, need_time = true;
+    let need_year = true,
+        need_time = true;
     if (format == 1) {
         need_time = false;
     } else if (format == 2) {
@@ -274,14 +291,14 @@ function paddingZero(v) {
     return v.length == 1 ? '0' + v : v;
 }
 
-// hit white list
+// hit project list
 function hitNotifyPro(curLogFolder, projects) {
-    return projects.some(x => curLogFolder.indexOf(x) > -1);
+    return projects.some((x) => curLogFolder.indexOf(x) > -1);
 }
 
-// hit black list
+// hit whilte/black list
 function filterList(curLogFolder, list) {
-    return list.some(x => curLogFolder.indexOf(x.split('#')[0]) > -1);
+    return list.some((x) => curLogFolder.endsWith(x.split('#')[0]));
 }
 
 function readSingleLog(filePath, descriptor) {
@@ -307,11 +324,13 @@ function BarkNotify(title, content) {
             timeout,
         };
 
-        axios.post(options.url, options)
+        axios
+            .post(options.url, options)
             .then(function (response) {
                 console.log('Bark Response:');
                 console.log(response.data);
-            }).catch(function (error) {
+            })
+            .catch(function (error) {
                 console.log('Bark Error Response:');
                 console.log(error);
             });
